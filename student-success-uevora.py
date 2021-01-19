beb593709aa354ed639a875c8f8fcb28f4b85e73
jperas243 <joaop.pereira@sapo.pt>
2021-01-19 18:42:46 +0000

    first final score

--- student-success-uevora.py
+++ student-success-uevora.py
@@ -10,16 +10,16 @@ from sklearn.preprocessing import LabelEncoder
 #models
 from sklearn.ensemble import GradientBoostingClassifier
 
-#validation libraries
-#from sklearn.cross_validation import StratifiedKFold
+#pred libraries
+#from sklearn.cross_pred import StratifiedKFold
 from IPython.display import display
 from sklearn import metrics
 
 def data_treatment(file):
 
     data = pd.read_csv(file, header=0)
-    print(file)
-    print(data)
+    #print(file)
+    #print(data)
 
     data.drop('Id',axis=1,inplace=True) 
    
@@ -33,56 +33,38 @@ def data_treatment(file):
 
     #split treino em x_train e y_train
     y_data = data[['Failure']].copy()
-    print(y_data)
+    #print(y_data)
 
     data.drop('Failure',axis=1,inplace=True)
     x_data = data.copy()
-    print(x_data)
+    #print(x_data)
 
     return x_data,y_data
 
+def best_parameters():
+    return 0;
 #----------------------------------main-------------------------------------#
-
+"""
+#merge sampleSubmission with test
 df_submission = pd.read_csv("sampleSubmission.csv", header=0)
 df_test = pd.read_csv("test.csv", header=0)
 
 Failures = df_submission["Failure"].copy()
-
-
-#print(Failures)
 df_test.insert(loc=31,column ='Failure',value=Failures)
-#print(df_test)
-df_test.to_csv(r'sub+test.csv', index = False)
+#df_test.to_csv(r'sub+test.csv', index = False)
+"""
 
+#data_treatment
 x_train,y_train = data_treatment('train.csv')
-x_test,y_test = data_treatment('sub+test.csv')
+#x_test,y_test = data_treatment('sub+test.csv')
+
+#split train to create pred data 
+x_train, x_pred, y_train, y_pred = train_test_split(x_train, y_train, random_state=1) #default test_size=25
 
 
-"""
-data=np.genfromtxt("train.csv", delimiter=",", dtype=None, encoding=None)
-x_train=data[1:,0:-1]    #  dados: da segunda à ultima linha, da primeira à penúltima coluna  
-y_train=data[1:,-1]      # classe: da segunda à ultima linha, só última coluna
 
-atributos = data[0,0:-1]
+clf = GradientBoostingClassifier(n_estimators=2000, max_depth=1)
+clf.fit(x_train, y_train.values.ravel())
 
-data=np.genfromtxt("test.csv", delimiter=",", dtype=None, encoding=None)
-x_test=data[1:,0:-1]    #  dados: da segunda à ultima linha, da primeira à penúltima coluna  
-y_test=data[1:,-1]   
-
-#validation data 
-x_train, x_validation, y_train, y_validation = train_test_split(x_train, y_train, random_state=1) #default test_size=25
-
-#encode non-numerical labels
-le = preprocessing.LabelEncoder()
-for row in x_train
-    le.fit(row)
-
-#clf = GradientBoostingClassifier()
-#clf.fit(x_train, y_train.values.ravel())
-
-
-#print(f"Atributos: {atributos}\n\n")
-#print(f"X_TRAIN: {x_train},\n\n Y_TRAIN: {y_train},\n\n")
-#print(f"X_VALIDATION: {x_validation},\n\n Y_VALIDATION: {y_validation},\n\n")
-#print(f"X_TEST {x_test},\n\n Y_TEST: {y_test}\n\n")
-"""
+final_score = clf.score(x_pred, y_pred) 
+print(f"final_score: {final_score*100}")
